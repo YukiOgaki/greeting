fc86b7ce3c8b7f18b548e92affd123f760724ef6
YukiOgaki <teamgift8@gmail.com>
2025-01-16 17:33:40 +0900

    課題修正3

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -8,32 +8,27 @@ class CommentsController extends Controller
 {
     public function comments($comment, $word = null) //$word = nullで、空白状態をデフォルト化
     {
-        if ($word == null) {
-            if ($comment != 'random') {
-                if ($comment == 'morning') {
-                    $h1 = '朝の';
-                    $h2 = 'おはようございます';
-                } elseif ($comment == 'afternoon') {
-                    $h1 = '昼の';
-                    $h2 = 'こんにちは';
-                } elseif ($comment == 'evening') {
-                    $h1 = '夕方の';
-                    $h2 = 'こんばんは';
-                } elseif ($comment == 'night') {
-                    $h1 = '夜の';
-                    $h2 = 'おやすみ';
-                } elseif ($comment == 'freeword') {
-                    $h1 = '自由な';
-                    $h2 = $word;
-                }
-                return view('message.comments', ['h1' => $h1, 'h2' => $h2]);
-            } elseif ($comment == 'random') {
-                $words = ['おはよう', 'こんにちは', 'こんばんは', 'おやすみ'];
-                $word = $words[array_rand($words)];
-                return view('message.comments', ['h1' => 'ランダムな', 'h2' => $word]);
-            }
-        } elseif ($comment == 'freeword' && $word != null) {
-            return view('message.comments', ['h1' => '自由な', 'h2' => $word]);
+        if ($comment == 'morning') {
+            $h1 = '朝の';
+            $h2 = 'おはようございます';
+        } elseif ($comment == 'afternoon') {
+            $h1 = '昼の';
+            $h2 = 'こんにちは';
+        } elseif ($comment == 'evening') {
+            $h1 = '夕方の';
+            $h2 = 'こんばんは';
+        } elseif ($comment == 'night') {
+            $h1 = '夜の';
+            $h2 = 'おやすみ';
+        } elseif ($comment == 'random') {
+            $words = ['おはよう', 'こんにちは', 'こんばんは', 'おやすみ'];
+            $word = $words[array_rand($words)];
+            $h1 = 'ランダムな';
+            $h2 = $word;
+        } elseif ($comment == 'freeword') {
+            $h1 = '自由な';
+            $h2 = $word;
         }
+        return view('message.comments', ['h1' => $h1, 'h2' => $h2]);
     }
 }
